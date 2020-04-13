60dc8aa0a02aebd6ff40a59368edec3e36998fc0
heydar <heydar.mahmoodi75@gmail.com>
2020-04-13 12:58:53 -0400

    extern caution

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #define PI 3.14159265   // it was usefull in C-style    but in C++ is better ways
 inline const double pi{3.14159265};    // C++ style , better way , always define it like this >> inline const
-
+//Caution: or you can use extern const double pi from another headers
 
 
 int main()
