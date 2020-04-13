0572b9fcb890ca198b5b7b3f24cce8b91094c094
heydar <heydar.mahmoodi75@gmail.com>
2020-04-13 12:09:16 -0400

    C-style way vs C++ way

--- main.cpp
+++ main.cpp
@@ -1,18 +1,13 @@
 #include<iostream>
-#define DEFINE_FUNC_PREPROCESSOR(NAME,COUNT,VALUE)\
-    void NAME##COUNT(){std::cout<<#VALUE;}
+#define PI 3.14159265   // it was usefull in C-style    but in C++ is better ways
+inline const double pi{3.14159265};    // C++ style , better way , always define it like this >> inline const
 
-DEFINE_FUNC_PREPROCESSOR(func,123,salam)
 
 
 int main()
 {
-    func123();
+    std::cout<<pi<<std::endl<<PI;
     std::cout<<std::endl;
     return 0;
 }
 
-
-// preprocessor define ...
-// #VALUE , in that # just is stringification operator.
-// ##COUNT , in that ## just is concatenate operator like + doing for variables.
